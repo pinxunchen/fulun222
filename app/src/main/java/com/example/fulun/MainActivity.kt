a9923388e810c1e44453c9f6d9544abdcaa6dad6
pinxunchen <apy9510@yahoo.com.tw>
2022-08-15 16:45:40 +0800

    8/15

--- app/src/main/java/com/example/fulun/MainActivity.kt
+++ app/src/main/java/com/example/fulun/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.fulun
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.example.fulun.data.UserInfo
+import com.example.fulun.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
@@ -12,9 +14,14 @@ import org.json.JSONObject
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         getUserData()
     }
 
@@ -24,6 +31,8 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun getUserData() {
+        binding.progressBar.visibility= View.VISIBLE
+
 //        okhttp設定
         val userdataUrl="https://raw.githubusercontent.com/Pinnnnnnnn/crwon/main/user_data.json"
         val okHttpClient = OkHttpClient().newBuilder().build()
@@ -33,16 +42,27 @@ class MainActivity : AppCompatActivity() {
         call.enqueue(object  :Callback{
             override fun onFailure(call: okhttp3.Call, e: IOException) {
                 Log.d("Pin","onFailure $e")
+
+            runOnUiThread {
+                binding.progressBar.visibility= View.GONE
+            }
             }
 
 
             override fun onResponse(call: okhttp3.Call , response: Response) {
+                var propertiesName = StringBuilder()
                 val userData = response.body?.string()
 //              userData拿資料後轉gson          (資料來源,轉換成xx格式)
                 val userInfo = Gson().fromJson(userData,UserInfo::class.java)
 
                 for(i in userInfo.features){
-                    Log.d("pin","個案姓名: ${i.properties.name},個案手機: ${i.properties.phone}")
+                    propertiesName.append(i.properties.name + "     "+i.properties.address+"\n")
+                }
+
+                runOnUiThread {
+                    binding.tvUserData.text = propertiesName
+                    binding.progressBar.visibility = View.GONE
+
                 }
 
 
